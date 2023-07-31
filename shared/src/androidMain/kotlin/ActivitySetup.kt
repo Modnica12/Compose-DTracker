d8c0c146acf2c4d70eca669bb058f1cf8cc39aa0
Kirill Shcherbakov <jcycu12@gmail.com>
2023-07-31 10:22:05 +0500

    refactor odyssey configuration

--- shared/src/androidMain/kotlin/ActivitySetup.kt
+++ shared/src/androidMain/kotlin/ActivitySetup.kt
@@ -1,6 +1,9 @@
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.core.view.WindowCompat
 import com.adeo.kviewmodel.odyssey.setupWithViewModels
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import navigation.NavigationTree
@@ -11,14 +14,13 @@ import ru.alexgladkov.odyssey.compose.local.LocalRootController
 import ru.alexgladkov.odyssey.compose.navigation.RootComposeBuilder
 import ru.alexgladkov.odyssey.compose.navigation.modal_navigation.ModalNavigator
 import ru.alexgladkov.odyssey.compose.navigation.modal_navigation.configuration.DefaultModalConfiguration
+import ru.alexgladkov.odyssey.compose.setup.OdysseyConfiguration
+import ru.alexgladkov.odyssey.compose.setup.StartScreen
 import ru.alexgladkov.odyssey.core.configuration.DisplayType
 import theme.AppTheme
 import theme.Theme.colors
 
 fun ComponentActivity.setupThemedNavigation() {
-    val rootController = RootComposeBuilder().apply { generateGraph() }.build()
-    rootController.setupWithActivity(this)
-    rootController.setupWithViewModels()
 
     setContent {
         AppTheme {
@@ -26,17 +28,46 @@ fun ComponentActivity.setupThemedNavigation() {
             val systemUiController = rememberSystemUiController()
             systemUiController.setSystemBarsColor(color = colors.primaryBackground)
 
-            val configuration = DefaultModalConfiguration(
+            val configuration = OdysseyConfiguration(
+                canvas = this,
                 backgroundColor = colors.primaryBackground,
-                displayType = DisplayType.EdgeToEdge
+                startScreen = StartScreen.Custom(NavigationTree.Tracker.List.name),
+                displayType = DisplayType.FullScreen
             )
 
-            CompositionLocalProvider(
-                LocalRootController provides rootController
-            ) {
-                ModalNavigator(configuration = configuration) {
-                    Navigator(startScreen = NavigationTree.Tracker.List.name)
-                }
+
+            setNavigationContent(configuration = configuration) {
+                generateGraph()
+            }
+        }
+    }
+}
+
+// Copied from Odessey to use integration with view models
+@Composable
+private fun setNavigationContent(configuration: OdysseyConfiguration, navigationGraph: RootComposeBuilder.() -> Unit) {
+    val rootController = RootComposeBuilder().apply(navigationGraph).build()
+    rootController.backgroundColor = configuration.backgroundColor
+    rootController.setupWithActivity(configuration.canvas)
+    rootController.setupWithViewModels()
+    rootController.onApplicationFinish = {}
+
+    when (configuration.displayType) {
+        is DisplayType.FullScreen -> {
+            WindowCompat.setDecorFitsSystemWindows(configuration.canvas.window, false)
+            configuration.canvas.window.statusBarColor = configuration.statusBarColor
+            configuration.canvas.window.navigationBarColor = configuration.navigationBarColor
+        }
+        else -> {}
+    }
+
+    CompositionLocalProvider(
+        LocalRootController provides rootController
+    ) {
+        ModalNavigator(configuration = DefaultModalConfiguration(configuration.backgroundColor, configuration.displayType)) {
+            when (val startScreen = configuration.startScreen) {
+                is StartScreen.Custom -> Navigator(startScreen = startScreen.startName)
+                StartScreen.First -> Navigator(startScreen = rootController.getFirstScreenName())
             }
         }
     }
--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -6,12 +6,14 @@ import ru.alexgladkov.odyssey.compose.setup.OdysseyConfiguration
 import ru.alexgladkov.odyssey.compose.setup.StartScreen
 import ru.alexgladkov.odyssey.compose.setup.setNavigationContent
 import theme.AppTheme
+import theme.Theme.colors
 
 fun MainViewController(): UIViewController =
     ComposeUIViewController {
         AppTheme {
             val odysseyConfiguration = OdysseyConfiguration(
-                startScreen = StartScreen.Custom(NavigationTree.Tracker.List.name)
+                startScreen = StartScreen.Custom(NavigationTree.Tracker.List.name),
+                backgroundColor = colors.primaryBackground
             )
             setNavigationContent(odysseyConfiguration) {
                 generateGraph()
