317333700166237768afa315dc633b835b95c258
Kirill Shcherbakov <jcycu12@gmail.com>
2023-08-17 16:34:12 +0500

    change colors and launcher icon

--- common/core-ui/src/commonMain/kotlin/theme/Color.kt
+++ common/core-ui/src/commonMain/kotlin/theme/Color.kt
@@ -16,25 +16,25 @@ data class TrackerColors(
 )
 
 internal val defaultPalette = TrackerColors(
-    primaryBackground = Color(0xFFE2E2E2),
+    primaryBackground = Color(0xFFF5F4F5),
     onPrimaryText = Color(0xFF080808),
-    primaryContainerBackground = Color(0xFFF2F2F2),
-    primaryContainerBorder = Color(0xFFD2D2D2),
+    primaryContainerBackground = Color.White,
+    primaryContainerBorder = Color(0xFFDFDFDF),
     onPrimaryContainerText = Color(0xFF0D0D0D),
     divider = Color(0xFFD8D8D8),
-    accent = Color(0xFFD00000),
-    accentContainer = Color(0xFFEECCCC),
+    accent = Color(0xFFE31E30),
+    accentContainer = Color(0xFFF9E9EA),
     errorColor = Color(0xFF9D0000)
 )
 
 internal val darkPalette = TrackerColors(
-    primaryBackground = Color(0xFF080808),
+    primaryBackground = Color(0xFF181818),
     onPrimaryText = Color(0xFFE2E2E2),
-    primaryContainerBackground = Color(0xFF181818),
+    primaryContainerBackground = Color.Black,
     primaryContainerBorder = Color(0xFF2F2F2F),
     onPrimaryContainerText = Color(0xFFF2F2F2),
     divider = Color(0xFF2D2D2D),
-    accent = Color(0xFFD00000),
+    accent = Color(0xFFE31E30),
     accentContainer = Color(0xFF500000),
     errorColor = Color(0xFF9D0000)
 )
