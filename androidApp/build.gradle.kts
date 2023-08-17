a9d875461fea6c2ba07b07a03089c3fdd33b2134
Kirill Shcherbakov <jcycu12@gmail.com>
2023-08-17 15:41:47 +0500

    change android app version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = AppInfo.id
         minSdk = (findProperty("android.minSdk") as String).toInt()
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10000
+        versionName = "1.0.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
